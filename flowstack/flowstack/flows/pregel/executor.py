89a5d7ebcc6d4656cfa221abbdb7625726bbe5b0
bensg <gabay67890@gmail.com>
2024-07-27 01:45:55 +0300

    BackgroundExecutor + AsyncBackgroundExecutor

--- flowstack/flowstack/flows/pregel/executor.py
+++ flowstack/flowstack/flows/pregel/executor.py
@@ -5,21 +5,34 @@ Credit to LangGraph - https://github.com/langchain-ai/langgraph/tree/main/langgr
 import asyncio
 import concurrent.futures
 from contextlib import ExitStack
+from contextvars import copy_context
 import sys
 from types import TracebackType
-from typing import AsyncContextManager, Callable, ContextManager, Optional, Protocol, Type
+from typing import AsyncContextManager, Awaitable, Callable, ContextManager, Optional, Protocol, Type
 
+from flowstack.flows.errors import GraphInterrupt
 from flowstack.utils.threading import get_executor
 
 class Submit[**P, T](Protocol):
     def __call__(
         self,
         function: Callable[P, T],
-        *args: P.args,
+        *args: *P.args,
         __name__: Optional[str] = None,
         __cancel_on_exit__: bool = False,
         **kwargs: P.kwargs
     ) -> concurrent.futures.Future[T]:
+        pass
+
+class AsyncSubmit[**P, T](Protocol):
+    def __call__(
+        self,
+        function: Callable[P, Awaitable[T]],
+        *args: *P.args,
+        __name__: Optional[str] = None,
+        __cancel_on_exit__: bool = False,
+        **kwargs: P.kwargs
+    ) -> asyncio.Task[T]:
         pass
 
 class BackgroundExecutor(ContextManager):
@@ -28,21 +41,13 @@ class BackgroundExecutor(ContextManager):
         self.executor = self.stack.enter_context(get_executor(max_workers=max_workers))
         self.tasks: dict[concurrent.futures.Future, bool] = {}
 
-    def __enter__(self) -> 'submit':
-        self.submit
+    def __enter__(self) -> Submit:
+        return self._submit
 
-    def __exit__(
-        self,
-        exc_type: Optional[Type[BaseException]],
-        exc_val: Optional[BaseException],
-        exc_tb: Optional[TracebackType]
-    ) -> Optional[bool]:
-        pass
-
-    def submit[**P, T](
+    def _submit[**P, T](
         self,
         function: Callable[P, T],
-        *args: P.args,
+        *args: *P.args,
         __name__: Optional[str] = None,
         __cancel_on_exit__: bool = False,
         **kwargs: P.kwargs
@@ -52,8 +57,40 @@ class BackgroundExecutor(ContextManager):
         task.add_done_callback(task)
         return task
 
-    def done(self, task: concurrent.futures.Future) -> None:
-        pass
+    def _done(self, task: concurrent.futures.Future) -> None:
+        try:
+            task.result()
+        except GraphInterrupt:
+            self.tasks.pop(task)
+        except BaseException:
+            pass
+        else:
+            self.tasks.pop(task)
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ) -> Optional[bool]:
+        # cancel all tasks that should be cancelled
+        for task, cancel in self.tasks.items():
+            if cancel:
+                task.cancel()
+        # wait for all tasks to finish
+        if tasks := {task for task in self.tasks if not task.done()}:
+            concurrent.futures.wait(tasks)
+        # shutdown the executor
+        self.stack.__exit__(exc_type, exc_val, exc_tb)
+        # re-raise the first exception that occured in a task
+        if exc_type is None:
+            # is there's already an exception being raised, don't raise another one
+            for task in self.tasks:
+                try:
+                    task.result()
+                except concurrent.futures.CancelledError:
+                    pass
+        return None
 
 class AsyncBackgroundExecutor(AsyncContextManager):
     def __init__(self):
@@ -61,10 +98,61 @@ class AsyncBackgroundExecutor(AsyncContextManager):
         self.context_not_supported = sys.version_info < (3, 11)
         self.sentinel = object()
 
+    async def __aenter__(self) -> AsyncSubmit:
+        return self._submit
+
+    def _submit[**P, T](
+        self,
+        function: Callable[P, Awaitable[T]],
+        *args: *P.args,
+        __name__: Optional[str] = None,
+        __cancel_on_exit__: bool = False,
+        **kwargs: P.kwargs
+    ) -> asyncio.Task[T]:
+        coro = function(*args, **kwargs)
+        if self.context_not_supported:
+            task = asyncio.create_task(coro, name=__name__)
+        else:
+            task = asyncio.create_task(coro, name=__name__, context=copy_context())
+        self.tasks[task] = __cancel_on_exit__
+        task.add_done_callback(self._done)
+        return task
+
+    def _done(self, task: asyncio.Task) -> None:
+        try:
+            task.result()
+        except GraphInterrupt:
+            self.tasks.pop(task)
+        except BaseException:
+            pass
+        else:
+            self.tasks.pop(task)
+
     async def __aexit__(
         self,
         exc_type: Optional[Type[BaseException]],
         exc_val: Optional[BaseException],
         exc_tb: Optional[TracebackType]
     ) -> Optional[bool]:
-        pass
+        return await asyncio.shield(self._exit(exc_type, exc_val, exc_tb))
+
+    async def _exit(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType]
+    ) -> None:
+        # cancel all tasks that should be cancelled
+        for task, cancel in self.tasks.items():
+            if cancel:
+                task.cancel(self.sentinel)
+        # wait for all tasks to finish
+        if tasks := {task for task in self.tasks if not task.done()}:
+            await asyncio.wait(tasks)
+        # re-raise the first exception that occured in a task
+        if exc_type:
+            for task in self.tasks:
+                try:
+                    task.result()
+                except asyncio.CancelledError:
+                    pass
