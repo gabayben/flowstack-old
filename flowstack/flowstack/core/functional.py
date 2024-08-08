f16df644144e221b73406c6131f4bcfddde3c327
bensg <gabay67890@gmail.com>
2024-08-09 01:32:32 +0300

    working on Functional

--- flowstack/flowstack/core/functional.py
+++ flowstack/flowstack/core/functional.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import AsyncIterator, Iterator, Optional, Type, TypeVar, override
 
 from pydantic import BaseModel
@@ -8,12 +9,47 @@ _Input = TypeVar('_Input')
 _Output = TypeVar('_Output')
 
 class Functional(Component[_Input, _Output]):
+    @property
+    @override
+    def InputType(self) -> Type[_Input]:
+        return self._parameter.annotation
+
+    @property
+    @override
+    def OutputType(self) -> Type[_Output]:
+        return self._signature.return_annotation
+
     def __init__(
         self,
         function: ComponentFunction[_Input, _Output],
+        name: Optional[str] = None,
         input_schema: Optional[Type[BaseModel]] = None,
         output_schema: Optional[Type[BaseModel]] = None
     ):
+        self._signature = inspect.signature(function)
+        parameters = {
+            key: value
+            for key, value in self._signature.parameters.items()
+            if value.annotation != inspect.Parameter.empty
+        }
+        if len(parameters) == 0:
+            raise ValueError(
+                'A function that is decorated with @component must have an input.'
+            )
+        self._parameter = list(parameters.values())[0]
+
+        self._function = function
+        self._input_schema = input_schema
+        self._output_schema = output_schema
+        super().__init__(name=name or function.__name__)
+
+    @override
+    def get_input_schema(self, **kwargs) -> Type[BaseModel]:
+        return self._input_schema or super().get_input_schema(**kwargs)
+
+    @override
+    def get_output_schema(self, **kwargs) -> Type[BaseModel]:
+        return self._output_schema or super().get_output_schema(**kwargs)
 
     def invoke(self, input: _Input, **kwargs) -> _Output:
         pass
